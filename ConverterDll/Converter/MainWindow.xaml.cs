678cad066b58fe6bf8e79bcec952913223875676
Widmosso <gilas.mateusz@gmail.com>
2015-09-07 14:39:23 +0200

    dodanie save and open file

--- ConverterDll/Converter/MainWindow.xaml.cs
+++ ConverterDll/Converter/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace Converter
 {
@@ -24,5 +25,61 @@ namespace Converter
 		{
 			InitializeComponent();
 		}
+
+	    private void BtnOpen_OnClick(object sender, RoutedEventArgs e)
+	    {
+	        OpenFileDialog openFile = new OpenFileDialog();
+	        openFile.DefaultExt = ".xlsx|.xls|.xlsm";
+	        openFile.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+	       
+	        Nullable<bool> result = openFile.ShowDialog();
+
+	        if (result == true)
+	        {
+	            string name = openFile.FileName;
+                txtPath.Text = name;
+	           
+	        }
+
+
+	    }
+
+        private void txtPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "XmlConvert";
+            dlg.DefaultExt = ".xml";
+            dlg.Filter = "XML Files (.xml)|*.xml";
+            Nullable<bool> result = dlg.ShowDialog();
+            if (result == true)
+            {
+                 string savePathname = dlg.FileName;
+                 txtSave.Text = savePathname;
+            }
+           
+        }
+
+        private void txtSave_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        
+        
 	}
 }
