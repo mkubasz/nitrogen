b7fe6f930b860e46386cbfcece324c88b9c446fb
Blacha <blacha14@gmail.com>
2015-09-09 21:30:27 +0200

    Szkolenie - Klasy i Konstruktory - kod

---
+++ Tematy_kody/KlasyKonstruktory/UnitTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using KlasyKonstruktory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Szkolenie obiekt = new Szkolenie();
+        }
+    }
+}
