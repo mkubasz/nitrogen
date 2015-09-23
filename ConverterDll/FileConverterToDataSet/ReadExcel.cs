a655b0cf9de822cb070941b9de8129c994f5aa08
TomaszF <eighttom@gmail.com>
2015-09-23 10:00:17 +0200

    add FileConverterToDataSet

---
+++ ConverterDll/FileConverterToDataSet/ReadExcel.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Data.OleDb;
+using System.Data;
+using FileConverterToDataSet.Interfaces;
+
+
+namespace FileConverterToDataSet
+{
+    class ReadExcel : ReadFile, IReadExcelFile
+    {
+        public DataSet ReadExcelFile(string pathToFile)
+        {
+            //DataSet dataSet = new DataSet();
+            using (OleDbConnection connection = new OleDbConnection(GetConnectionString(pathToFile)))
+            {
+                connection.Open();
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                //sheets names ascending orders
+                DataTable dataTableSheet = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+                if (dataTableSheet != null)
+                    foreach (string sheetName in dataTableSheet.Rows.Cast<DataRow>().Select(dataRow => dataRow["TABLE_NAME"].ToString()).Where(sheetName => sheetName.EndsWith("$")))
+                    {
+                        command.CommandText = "SELECT * FROM [" + sheetName + "]";
+                        DataTable dataTable = new DataTable { TableName = sheetName };
+                        OleDbDataAdapter dataAdapter = new OleDbDataAdapter(command);
+                        dataAdapter.Fill(dataTable);
+                        DataSet.Tables.Add(dataTable);
+                    }
+                connection.Close();
+            }
+            return DataSet;
+        }
+    }
+}
---
+++ ConverterDll/FileConverterToDataSet/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileConverterToDataSet
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Dictionary<string, string> _fileCheckDictionary = new Dictionary<string, string>()
+        {
+            {".xlsx", "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=\"Excel 12.0;HDR=Yes;IMEX=1\""},
+            {"xls", "Provider=Microsoft.Jet.OLEDB.4.0;Data Source={0};Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=1\"" }
+        };
+            string path = @"C:\nowy.xlsx";
+
+            ReadExcel excel = new ReadExcel();
+
+            string a = excel.GetConnectionString(path);
+            excel.ReadExcelFile(path);
+            //     excel.ReadExcelFile(path);
+            //   Assert.AreEqual("a", "a");
+
+        }
+    }
+
+
+}
