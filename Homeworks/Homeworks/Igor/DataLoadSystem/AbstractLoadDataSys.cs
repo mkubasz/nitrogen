d941a612d9c0e15950f43b9f2bbed10f1ed8e72a
Igor Santarek <jegor377@gmail.com>
2015-09-26 01:02:18 +0200

    save

---
+++ Homeworks/Homeworks/Igor/DataLoadSystem/AbstractLoadDataSys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks.Igor.DataLoadSystem
+{
+    interface AbstractLoadDataSys
+    {
+        HashSet<string> GetData();
+    }
+}
---
+++ Homeworks/Homeworks/Igor/DataLoadSystem/XMLDataLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks.Igor.DataLoadSystem
+{
+    internal class XMLDataLoader : AbstractLoadDataSys
+    {
+        HashSet<string> data = new HashSet<string>();
+
+        public XMLDataLoader(string path)
+        {
+
+        }
+
+        HashSet<string> GetData()
+        {
+            return this.data;
+        }
+    }
+}
