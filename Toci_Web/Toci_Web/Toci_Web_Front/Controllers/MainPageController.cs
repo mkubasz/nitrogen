5dc84a4450ff63c85d5bf839316efc63ce85b323
Bolec <subotkowski@gmail.com>
2015-10-05 21:53:54 +0200

    scratch of membersarea

---
+++ Toci_Web/Toci_Web/Toci_Web_Front/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Toci_Web_Front.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}
