4f4552859576b93152eb4b2c77fe6b039bb3f645
brzydalek <marcin.mazurowski@o2.pl>
2015-10-10 17:59:53 +0200

    DB repair

---
+++ SphynxWeb/Repozytorium/Migrations/201510101557526_repairs.cs
@@ -0,0 +1,36 @@
+namespace Repozytorium.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class repairs : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.BlogModels", newName: "BlogModel");
+            RenameTable(name: "dbo.FotoModels", newName: "FotoModel");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            AddForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles", "Id");
+            AddForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers", "Id");
+            AddForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers", "Id");
+            AddForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
+            AddForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers", "Id", cascadeDelete: true);
+            AddForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles", "Id", cascadeDelete: true);
+            RenameTable(name: "dbo.FotoModel", newName: "FotoModels");
+            RenameTable(name: "dbo.BlogModel", newName: "BlogModels");
+        }
+    }
+}
--- SphynxWeb/Repozytorium/Models/ApplicationDbContext.cs
+++ SphynxWeb/Repozytorium/Models/ApplicationDbContext.cs
@@ -21,18 +21,19 @@ namespace Repozytorium.Models
         }
 
         public IDbSet<FotoModel> Fotos { get; set; }
-        //public IDbSet<BlogModel> Posts { get; set; }
+        public IDbSet<BlogModel> Posts { get; set; }
 
-        public System.Data.Entity.DbSet<Repozytorium.Models.BlogModel> BlogModels { get; set; }
+        //public System.Data.Entity.DbSet<Repozytorium.Models.BlogModel> BlogModels { get; set; }
 
-        public System.Data.Entity.DbSet<Repozytorium.Models.FotoModel> FotoModels { get; set; }
+       //public System.Data.Entity.DbSet<Repozytorium.Models.FotoModel> FotoModels { get; set; }
 
 
-       // protected override void OnModelCreating(DbModelBuilder modelBuilder)
-       // {
-       //base.OnModelCreating(modelBuilder);
-       //     modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
         
-       // }
+        }
     }
 }
