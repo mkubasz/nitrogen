1b7d19ff53da8c20e1233cba24c76fe72ab4c232
brzydalek <marcin.mazurowski@o2.pl>
2015-10-11 18:43:58 +0200

    changing js

--- SphynxWeb/SphynxWeb/Scripts/js/custom.js
+++ SphynxWeb/SphynxWeb/Scripts/js/custom.js
@@ -29,7 +29,7 @@ jQuery(document).ready(function() {
                       menu = target;
                   $target = $(target);
                   $('html, body').stop().animate({
-                      'scrollTop': $target.offset().top+2
+                      'scrollTop': $target.offset().top-15
                   }, 500, 'swing', function () {
                       window.location.hash = target;
                       $(document).on("scroll", onScroll);
