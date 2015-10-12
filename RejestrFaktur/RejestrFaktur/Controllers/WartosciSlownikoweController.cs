6ef9d0efda79da926eee8466adea9b2d5465ce57
Marcin Guszpit <marcin.guszpit@gmail.com>
2015-10-12 20:03:15 +0200

    Zrobienie podstaw mojego kontrolera i prostego szablonu do moich widoków

---
+++ RejestrFaktur/RejestrFaktur/Controllers/WartosciSlownikoweController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RejestrFaktur.Controllers
+{
+    public class WartosciSlownikoweController : Controller
+    {
+        // GET: WartosciSlownikowe
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Wartości słownikowe";
+            return View();
+        }
+
+        // GET: WartosciSlownikowe
+        public ActionResult JednostkiMiary()
+        {
+            ViewBag.Title = "Jednostki miary";
+            return View();
+        }
+
+        // GET: WartosciSlownikowe
+        public ActionResult StawkiPodatku()
+        {
+            ViewBag.Title = "Stawki podatku";
+            return View();
+        }
+
+
+        // GET: WartosciSlownikowe
+        public ActionResult Waluty()
+        {
+            ViewBag.Title = "Waluty";
+            return View();
+        }
+
+        // GET: WartosciSlownikowe
+        public ActionResult TypyPlatnosci()
+        {
+            ViewBag.Title = "Typy płatności";
+            return View();
+        }
+
+    }
+}
