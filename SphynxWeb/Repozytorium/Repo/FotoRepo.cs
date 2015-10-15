0a397d6a2a4456b619d44e4550195da92ababe86
TomaszF <tomaszfrosztega@gmail.com>
2015-10-15 13:39:55 +0200

    adding FotoModelView, methods in FotoRepo, simple view in index2 and target view at index

--- SphynxWeb/Repozytorium/Repo/FotoRepo.cs
+++ SphynxWeb/Repozytorium/Repo/FotoRepo.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Web;
 using Repozytorium.Models;
+using Repozytorium.Models.ViewModel;
 
 namespace Repozytorium.Repo
 {
@@ -23,6 +27,7 @@ namespace Repozytorium.Repo
         public void EditFotoModel(FotoModel foto)
         {
             db.Entry(foto).State = EntityState.Modified;
+            db.SaveChanges();
         }
 
         public bool DeleteFoto(int id)
@@ -41,9 +46,83 @@ namespace Repozytorium.Repo
             
         }
 
-        public void AddFoto(FotoModel foto)
+        //public void AddFoto(FotoModel foto)
+        //{
+        //    db.Fotos.Add(foto);
+        //    db.SaveChanges();
+        //}
+
+        //public byte[] GetImageBytes(int id)
+        //{
+        //    var cos = db.Fotos.Where(img => img.FotoId == id).Select(img => img.FotoData).First();
+        //    return cos;
+        //}
+
+        public int UploadImageInDataBase(HttpPostedFileBase file, FotoModel fotoModel)
         {
+            //    fotoModel.FotoData = ConvertToBytes(file);
+            Image sourceimage = Image.FromStream(file.InputStream);
+            fotoModel.FotoData = ConvertImageToBytes(sourceimage);
+
+            var min = ScaleImage(sourceimage, 300, 300);
+            fotoModel.FotoMiniatureData = ConvertImageToBytes(min);
+
+            var foto = new FotoModel
+            {
+                FotoSize = file.ContentLength,
+                Description = fotoModel.Description,
+                FileName = fotoModel.FileName,
+                FotoData = fotoModel.FotoData,
+                FotoMiniatureData = fotoModel.FotoMiniatureData
+            };
             db.Fotos.Add(foto);
+            int i = db.SaveChanges();
+            return i == 1 ? 1 : 0;
+        }
+
+        public static byte[] ConvertImageToBytes(Image img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+
+        static public Bitmap ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+            Graphics.FromImage(newImage).DrawImage(image, 0, 0, newWidth, newHeight);
+            Bitmap bmp = new Bitmap(newImage);
+
+            return bmp;
+        }
+
+        public List<FotoModelView> DisplayFotoModelViews(IQueryable<FotoModel> fotoModel)
+        {
+            List<FotoModelView> viewFotos = new List<FotoModelView>();
+            foreach (var item in fotoModel)
+            {
+                viewFotos.Add(
+
+                    new FotoModelView
+                    {
+                        FotoId = item.FotoId,
+                        FotoSize = item.FotoSize,
+                        Description = item.Description,
+                        FileName = item.FileName,
+                        FotoData = "data:image/png;base64," + Convert.ToBase64String(item.FotoData),
+                        FotoMiniatureData = "data:image/png;base64," + Convert.ToBase64String(item.FotoMiniatureData)
+                    }
+                );
+            }
+
+
+            return viewFotos;
         }
     }
 }
