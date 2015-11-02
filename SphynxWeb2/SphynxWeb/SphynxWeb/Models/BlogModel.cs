9b43c50a3840d958057fa031bc3e5cf2dd6ebc16
brzydalek <marcin.mazurowski@o2.pl>
2015-11-02 21:10:40 +0100

    poprawki

--- SphynxWeb2/SphynxWeb/SphynxWeb/Models/BlogModel.cs
+++ SphynxWeb2/SphynxWeb/SphynxWeb/Models/BlogModel.cs
@@ -7,9 +7,10 @@ namespace SphynxWeb.Models
     {
         [Key]
         public int PostId { get; set; }
-        [MaxLength(450, ErrorMessage = "Your pust must be max {0} character long"), MinLength(5)] 
+        [MaxLength(4000, ErrorMessage = "Your pust must be max {0} character long"), MinLength(5)] 
         [DataType(DataType.MultilineText)]
         public string Post { get; set; }
+        [MaxLength(4000, ErrorMessage = "Your pust must be max {0} character long"), MinLength(5)]
         public string Name { get; set; }
         public bool ShowOnStartPage { get; set; }
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
